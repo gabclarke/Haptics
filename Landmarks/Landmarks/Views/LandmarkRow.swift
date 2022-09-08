aa910145e0635567c5f5206fd15ecca7ea5c5559
gabclarke <gabyclarke@me.com>
2022-09-08 10:59:54 -0700

    Tutorial part 2, rebuild

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Gabrielle Clarke on 9/8/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
