8b9316718ea4eb78d630a9885d9cd3b1c42aa8fc
gabclarke <gabyclarke@me.com>
2022-09-08 11:16:20 -0700

    from hardcoded to dynamic data

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Gabrielle Clarke on 9/8/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
