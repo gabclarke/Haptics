148498ae323dc3cc611c2219c68134c2ea9ca408
gabclarke <gabyclarke@me.com>
2022-09-08 09:15:25 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gabrielle Clarke on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
