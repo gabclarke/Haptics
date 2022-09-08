705e71700c94e0b31505343e94c60f17ec12e662
gabclarke <gabyclarke@me.com>
2022-09-08 11:48:37 -0700

    Tutorial pt. 2 complete

--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -8,44 +8,47 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-                    MapView()
-                        .ignoresSafeArea(edges: .top)
-                        .frame(height: 300)
-                    
-                    CircleImage()
-                        .offset(y: -130)
-                        .padding(.bottom, -130)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Turtle Rock")
-                            .font(.title)
-                        HStack {
-                            Text("Joshua Tree National Park")
-                                .font(.subheadline)
-                            Spacer()
-                            Text("California")
-                                .font(.subheadline)
-                        }
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
                         .font(.subheadline)
-                        .foregroundColor(.secondary)
-                        
-                        Divider()
-                        
-                        Text("About Turtle Rock")
-                            .font(.title2)
-                        Text("Descriptive text goes here.")
-                    }
-                    .padding()
-                    
                     Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+            
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
